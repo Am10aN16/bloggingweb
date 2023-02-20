5bb402060a39353728b64fce6b611ae0a2616820
Am10aN16 <2020071013@mmmut.ac.in>
2023-02-20 14:38:39 +0530

    first

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <>
     <nav className="navbar navbar-expand-lg navbar-light purple">
   <div className="container-fluid">
-    <a className="navbar-brand white" href="https">Blogger's Tweet</a>
+    <a className="navbar-brand white" href="/">Blogger's Tweet</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
