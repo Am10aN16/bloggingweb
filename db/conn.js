562edb3165394109113bd8f6b1ad3ebba08f236c
Am10aN16 <2020071013@mmmut.ac.in>
2023-02-20 12:59:49 +0530

    first

--- db/conn.js
+++ db/conn.js
@@ -4,7 +4,8 @@ const DB = process.env.DATABASE;
 const dbconnection = async() => {
   try {
    await mongoose.connect(DB , {
- 
+    useNewUrlParser:true,
+    useUnifiedTopology:true
    });
    console.log(`Connection Success`);
   } catch (error) {
